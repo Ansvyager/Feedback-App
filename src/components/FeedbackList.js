7d018788871fd24449a46268b2ceb91adbd9aac6
Ansvyager <Ansvyager@gmail.com>
2022-04-25 06:10:35 +0700

    added animate

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import React from "react";
 import FeedBackItem from "./FeedBackItem";
+import { motion, AnimatePresence } from "framer-motion";
 function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>no feedback yet</p>;
@@ -8,18 +9,38 @@ function FeedbackList({ feedback, handleDelete }) {
 
   return (
     <div className="feedback-list">
-      {feedback.map((item) => (
-        <FeedBackItem
-          handleDelete={handleDelete}
-          item={item}
-          key={item.id}
-        >
-          {item}
-        </FeedBackItem>
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedBackItem handleDelete={handleDelete} item={item} key={item.id}>
+              {item}
+            </FeedBackItem>
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
 }
+
+//   return (
+//     <div className="feedback-list">
+//       {feedback.map((item) => (
+//         <FeedBackItem
+//           handleDelete={handleDelete}
+//           item={item}
+//           key={item.id}
+//         >
+//           {item}
+//         </FeedBackItem>
+//       ))}
+//     </div>
+//   );
+// }
 
 FeedbackList.propTypes = {
   feedback: PropTypes.arrayOf(

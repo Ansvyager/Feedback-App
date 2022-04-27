00fec9c7377e6c37600d14332c5e56f9c00baa5f
Ansvyager <Ansvyager@gmail.com>
2022-04-28 01:49:38 +0700

    change

--- src/components/FeedbackStats.js
+++ src/components/FeedbackStats.js
@@ -1,16 +1,18 @@
-
-import {useContext} from "react";
+import { useContext } from "react";
 import FeedbackContext from "../context/FeedbackContext";
-function FeedbackStats() {
-    const { feedback } = useContext(FeedbackContext);
-  // calculate ratings avg
-  let average =
-    feedback.reduce((acc, cur) => {
-      return acc + cur.rating;
-    }, 0) / feedback.length;
 
-  average = average.toFixed(1).replace(/[.,]$/, "");
- 
+function FeedbackStats() {
+  const { feedback } = useContext(FeedbackContext);
+
+  // NOTE: simpler average calculation
+  // Calculate ratings avg
+  const average = Math.round(
+    feedback.reduce((acc, { rating }) => acc + rating, 0) / feedback.length
+  );
+
+  // average = average.toFixed(1).replace(/[.,]0$/, '')
+  // average = Math.round(average)
+
   return (
     <div className="feedback-stats">
       <h4>{feedback.length} Reviews</h4>
@@ -18,7 +20,5 @@ function FeedbackStats() {
     </div>
   );
 }
-
-
 
 export default FeedbackStats;
